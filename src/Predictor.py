77c23daa79ea1eaeb9e1f52be2cd071db49b0dbe
wgm1001 <44786081+wgm1001@users.noreply.github.com>
2020-07-09 14:20:40 +0200

    Corrgido de nuevo problema con las clase predictor
    Esto es porque moví un método y se me olvido quitarlo del primero

--- src/Predictor.py
+++ src/Predictor.py
@@ -11,16 +11,8 @@ class Predictor:
     def __init__(self,modelo='SVM',random_state=None):
         self.modelo=modelo
         if modelo=='SVM':
-            trans=TranscriptorSVM.transcribir(repositorios=repositorios,stopW=stopW,idioma=idioma,comentarios=comentarios)
-            self.X=trans[0]
-            self.y=trans[1]
-            self.labels=trans[2]
             self.clf=ModeloSVM(random_state=random_state)
         if modelo=='MultinomialNB':
-            trans=TranscriptorMultinomialNB.transcribir(repositorios=repositorios,stopW=stopW,idioma=idioma,comentarios=comentarios)
-            self.X=trans[0]
-            self.y=trans[1]
-            self.labels=None
             self.clf=ModeloSVM()
     def entrenar(self,stopW=True,idioma='english',comentarios=True,repositorios=[],):
         if self.modelo=='SVM':
